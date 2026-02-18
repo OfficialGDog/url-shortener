c881213e1aa4510c40d2e681d9a3fca02b33ad34
Gareth <garethdavieslive@gmail.com>
2026-02-18 16:42:14 +0000

    Initial project setup with Express and TypeScript

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+const PORT = 3004;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(PORT, () => {
+console.log(`Server is running on http://localhost:${PORT}`);
+});
